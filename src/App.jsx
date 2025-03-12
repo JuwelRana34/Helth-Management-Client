d3129bd357539f6f74a2eada105e17cac1487070
ataurwd <ataurrahman24707@gmail.com>
2025-03-12 11:15:04 +0600

    add folder structure and also add some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>welcome to our website</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, corrupti magnam iste accusamus totam, tenetur beatae cupiditate nemo maxime optio odio itaque ratione distinctio quasi officiis suscipit fugit eveniet. Ipsam.</p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        corrupti magnam iste accusamus totam, tenetur beatae cupiditate nemo
+        maxime optio odio itaque ratione distinctio quasi officiis suscipit
+        fugit eveniet. Ipsam.
+      </p>
       <h3>new update</h3>
       <h3>new something</h3>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
