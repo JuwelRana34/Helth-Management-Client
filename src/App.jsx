144cb3ccec8dc42b81f3c7915efc9417cf98fafe
rahat0078 <rarahat1710@gmail.com>
2025-03-12 11:27:14 +0600

    update from rahat

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,10 @@ function App() {
   return (
     <>
       <h1>welcome to our website</h1>
-      <h2> online hospital management system </h2>
+      <h2> online hospital management system rahat 0078 </h2>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, corrupti magnam iste accusamus totam, tenetur beatae cupiditate nemo maxime optio odio itaque ratione distinctio quasi officiis suscipit fugit eveniet. Ipsam.</p>
       <h3>new update</h3>
       <h3>new something</h3>
-
     </>
   )
 }
