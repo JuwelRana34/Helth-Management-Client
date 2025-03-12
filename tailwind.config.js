1080152ed2293802e4ea37bcd0db1b804829d6a3
JuwelRana34 <juwelrana3426@gmail.com>
2025-03-12 17:42:36 +0600

    Update HomeCard component and styles for health services display

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1C5CBB',
+        secondary: '#39B5A4',
+    },
+    },
   },
   plugins: [
-    require('daisyui'),
+    daisyui
   ],
 }
 
