9f8a607ec5747434690634d82ad15324c7b4705c
alaminislamrahat <alaminislamrahat@gmail.com>
2025-03-12 12:05:09 +0600

    Basic routing setup done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from '../layout/Layout';
+import Home from '../pages/Home';
 
 const Routes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const route = createBrowserRouter([
+        {
+            path : '/',
+            element : <Layout/>,
+            children : [
+                {
+                    path : '/',
+                    element : <Home/>
+                }
+            ]
+        }
+    ])
+    return <RouterProvider router={route} />
 };
 
 export default Routes;
