9411ab391125a70018cd22457e98f3c3d30e1595
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-03-13 20:15:17 +0600

    Add ServiceSection component to Home page and adjust layout

--- src/components/section/ServiceSection.jsx
+++ src/components/section/ServiceSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ServiceSection = () => {
     return (
-        <div className='bg-yellow-300 flex flex-col items-center justify-center gap-5 py-20 text-center my-10'>
+        <div className='bg-yellow-300 flex flex-col items-center justify-center gap-5 py-20 text-center '>
             <p className='font-bold'>Join Our Dental Family</p>
             <p className='font-bold text-6xl'>Schedule Your Dental <br /> Exam With Us!</p>
             <p className='text-gray-600'>Take the step towards a healthier smile: schedule you personalized <br /> consultation with our dental expert dental team tody.</p>
