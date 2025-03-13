77cb039fc458503f65bac61b2a23f15c9205bee7
rahat0078 <rarahat1710@gmail.com>
2025-03-13 23:36:01 +0600

    Doctors Section Done

--- src/pages/about-us/About.jsx
+++ src/pages/about-us/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aboutus_section1 from './Aboutus_ection1';
 import TestimonialSection from './Testimonials';
+import DoctorsSection from './DoctorsSection';
 
 const About = () => {
     return (
         <div>
            <Aboutus_section1/>
+           <DoctorsSection/>
            <TestimonialSection/>
         </div>
     );
