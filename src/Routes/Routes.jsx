1d57d87ed5a6c69f7f10fc5d61a5bf3a6a9cb240
ataurwd <ataurrahman24707@gmail.com>
2025-03-13 09:56:37 +0600

    update navbar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import Layout from '../layout/Layout';
 import Home from '../pages/Home';
+import About from '../pages/about-us/About';
+import Services from '../pages/services/Services';
 
 const Routes = () => {
     const route = createBrowserRouter([
@@ -12,6 +14,14 @@ const Routes = () => {
                 {
                     path : '/',
                     element : <Home/>
+                },
+                {
+                    path : '/about',
+                    element : <About/>
+                },
+                {
+                    path : '/services',
+                    element : <Services/>
                 },
             ]
         },
