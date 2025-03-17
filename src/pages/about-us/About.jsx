108a6353c4784803da83ff0eb078a31f6a4f2c16
rahat0078 <rarahat1710@gmail.com>
2025-03-17 11:27:16 +0600

    add a new section in  about us page

--- src/pages/about-us/About.jsx
+++ src/pages/about-us/About.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import Aboutus_section1 from './Aboutus_ection1';
 import TestimonialSection from './Testimonials';
-import DoctorsSection from './DoctorsSection';
 import AboutUs1 from './AboutUs1';
 
 const About = () => {
     return (
         <div>
            <AboutUs1/>
-           <DoctorsSection/>
            <TestimonialSection/>
         </div>
     );
