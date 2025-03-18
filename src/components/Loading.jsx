1410474fe619c6baf5b4c5fef5d3b592121ca394
Ataur Rahman <ataurrahman24707@gmail.com>
2025-03-18 23:07:37 +0600

    Ataur (#65)
    * update the env file
    
    * update dashboard desing and make it look good and professional

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,9 @@
 import loading_gif from '../assets/loading.gif';
-
+import { HashLoader } from "react-spinners";
 const Loading = () => {
     return (
         <div className="flex justify-center min-h-screen items-center">
-            <img src={loading_gif} alt="" />
+        <HashLoader size={70} color='#1C5CBB'/>
         </div>
     );
 };
