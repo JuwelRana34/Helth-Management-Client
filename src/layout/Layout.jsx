d4e7ff96ca94d2f332c9bd52c3f9be6339c57ff5
Ataur Rahman <ataurrahman24707@gmail.com>
2025-03-18 22:11:34 +0600

    Rahat0078 (#63)
    * private route added
    
    * loading added
    
    ---------
    
    Co-authored-by: rahat0078 <rarahat1710@gmail.com>

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../components/Footer';
+import useAuth from '../Hooks/useAuth';
+import Loading from '../components/Loading';
 
 const Layout = () => {
+
+    const {loading} = useAuth()
+
+    if (loading) {
+        return <div> <Loading/> </div>
+
+    }
+
+
     return (
         <div className='font-roboto'>
             <Navbar />
