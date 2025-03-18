7d3e31428844fa46fe0d9a61c00f9d30c5bf77c0
Ataur Rahman <ataurrahman24707@gmail.com>
2025-03-19 00:04:24 +0600

    solve navbar and fixed some eassy
    * update the env file
    
    * update dashboard desing and make it look good and professional
    
    * solve navbar and fixed some eassy

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -52,35 +52,31 @@ const AuthProvider = ({ children }) => {
     setDarkMode(checked);
   };
 
-    useEffect(() => {
-        const unsubsribe = onAuthStateChanged(auth, currentUser => {
+  useEffect(() => {
+    const unsubsribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        setLoading(false);
+      }
 
-            setUser(currentUser);
-            if(currentUser){
-                setLoading(false)
-            }
-         
-            // console.log(currentUser);
-            if(currentUser){
-                // get token and store client
-                const userInfo = {
-                    email : currentUser.email
-                }
-               
-                .then(res => {
-                    if(res.data.token){
-                        localStorage.setItem('access-token',res.data.token)
-                        setLoading(false)
-                    }
-                })
-            }
-            else{
-                // do something
-                localStorage.removeItem('access-token')
-            }
-            
+      // console.log(currentUser);
+      if (currentUser) {
+        // get token and store client
+        const userInfo = {
+          email: currentUser.email,
+        }.then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
             setLoading(false);
-        })
+          }
+        });
+      } else {
+        // do something
+        localStorage.removeItem("access-token");
+      }
+
+      setLoading(false);
+    });
 
     return () => {
       return unsubsribe();
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,9 @@
 import loading_gif from '../assets/loading.gif';
-
+import { HashLoader } from "react-spinners";
 const Loading = () => {
     return (
         <div className="flex justify-center min-h-screen items-center">
             <img src={loading_gif} alt="" />
-            <div>problem</div>
         </div>
     );
 };
