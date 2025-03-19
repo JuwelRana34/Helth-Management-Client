be7c812f97428d6be05247df1ba04d4be9dd1846
Ruhul Amin Rahat <rarahat1710@gmail.com>
2025-03-19 14:28:32 +0600

    Rahat0078 (#70)
    * private route added
    
    * loading added
    
    * dark theme added
    
    * solve issue

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,10 @@
 import loading_gif from '../assets/loading.gif';
-import { HashLoader } from "react-spinners";
+
 const Loading = () => {
     return (
         <div className="flex justify-center min-h-screen items-center">
             <img src={loading_gif} alt="" />
+            <div>problem</div>
         </div>
     );
 };
