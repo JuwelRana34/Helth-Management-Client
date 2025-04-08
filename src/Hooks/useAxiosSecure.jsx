69c6a17cbe17d5f3a62c2fb9cc6dcebbdeab71b7
alaminislamrahat <alaminislamrahat@gmail.com>
2025-04-08 20:09:40 +0600

    jwt added

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,39 +1,34 @@
-import axios from "axios";
-import useAuth from "./useAuth";
-import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 
+import { useNavigate } from 'react-router-dom';
+import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
-    baseURL : import.meta.env.VITE_Url
-})
+    baseURL: 'http://localhost:5000',
+    withCredentials: true
+});
+
 const useAxiosSecure = () => {
     const { logOut } = useAuth();
     const navigate = useNavigate()
-    axiosSecure.interceptors.request.use(function (config) {
-        const token = localStorage.getItem('access-token');
-        // console.log('iterceptor', token)
-        config.headers.authorization = `Bearer ${token}`;
-        return config;
-    }, function (error) {
-        return Promise.reject(error)
-    })
+    //interseptors
+    axiosSecure.interceptors.response.use(
+        res => {
 
+            return res;
+        },
+        async error => {
 
-    // response interceptors 
-    axiosSecure.interceptors.response.use(function (response) {
-
-        return response;
-    }, async function (error) {
-        const status = error.response.status;
-        // console.log('in the interceptor error', status)
-        if (status === 401 || status === 403) {
-            await logOut();
-            navigate('/login')
+            if (error.response.status === 401 || error.response.status === 403) {
+                await logOut();
+                navigate('/login')
+            }
+            return Promise.reject(error)
         }
-        return Promise.reject(error);
-    });
-    return axiosSecure;
+    )
+
+    return axiosSecure; // Return the customized axios instance
 };
 
 export default useAxiosSecure;
